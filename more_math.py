d4642d0041d958d728d7e661fb9a7a5332ae4cfb
pp9483 <pp9483@g.rit.edu>
2025-09-21 18:37:38 +0200

    6/7

--- more_math.py
+++ more_math.py
@@ -1,13 +1,44 @@
 '''
-@ASSESSME.USERID: userID
-@ASSESSME.AUTHOR: author, list of authors
+@ASSESSME.USERID: pp9483
+@ASSESSME.AUTHOR: Patrik Pavlovic
 @ASSESSME.DESCRIPTION: Assignment 3.1
 @ASSESSME.ANALYZE: YES
 '''
+import math
 
+def fact(num):
+    num=int(num)
+    if num <= 0:
+        return 0
+    else:
+        return math.factorial(num)
+
+def root(num):
+   num = int(num)
+   if num <= 0:
+       return 0
+   else:
+       return math.sqrt(num)
+   
+
+def trunk(num):
+   num = int(num)
+   if num <= 0:
+       return 0
+   else:
+       return math.trunc(num)
+       
+    
 
 def main():
-   pass
+   num = input("Please enter a numeric value:")
+   print(num,"factorial =",fact(num))
+   num1 = float(input("Please enter a numeric value:"))
+   print("The square root of",num1,"is",root(num1))
+   num2 = float(input("Please enter a numeric value:"))
+   print(num2,"truncated =",trunk(num2))
+
+   
 
 
 if __name__ == "__main__":
